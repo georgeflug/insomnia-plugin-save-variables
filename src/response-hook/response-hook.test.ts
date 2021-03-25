2d19bf7c34d6646c4d305cb8b607a43b1429750e
Richard White <georgeflug@gmail.com>
2021-03-24 20:06:37 -0500

    GH-8. Set variable during response using jsonpath

--- src/response-hook/response-hook.test.ts
+++ src/response-hook/response-hook.test.ts
@@ -1,23 +1,64 @@
+import { VariableDefinition } from '../custom-header-format/variable-definition'
 import { variableSavingResponseHook } from './response-hook'
 
 describe('Variable Saving Response Hook', () => {
   const getStatusCodeMock = jest.fn()
+  const getBodyMock = jest.fn()
+  const storeGetItemMock = jest.fn()
   const storeSetItemMock = jest.fn()
+  const storeRemoveItemMock = jest.fn()
   const context = ({
     response: ({
+      getBody: getBodyMock,
       getStatusCode: getStatusCodeMock,
     } as Partial<Insomnia.ResponseContext>) as Insomnia.ResponseContext,
     store: ({
+      getItem: storeGetItemMock,
       setItem: storeSetItemMock,
+      removeItem: storeRemoveItemMock,
     } as Partial<Insomnia.StoreContext>) as Insomnia.StoreContext,
   } as Partial<Insomnia.ResponseHookContext>) as Insomnia.ResponseHookContext
 
-  it('should save the status code of the response into a variable', async () => {
-    const statusCode = 404
-    getStatusCodeMock.mockReturnValue(statusCode)
+  it('should read the response body value into a variable using json path', async () => {
+    const VariableDefinition: VariableDefinition = {
+      variableName: 'ticket',
+      jsonPath: '$.ticketId',
+    }
+    const body = {
+      someValue: 'test',
+      ticketId: '123',
+    }
+    storeGetItemMock.mockResolvedValue(JSON.stringify([VariableDefinition]))
+    getBodyMock.mockReturnValue(JSON.stringify(body))
 
     await variableSavingResponseHook(context)
 
-    expect(storeSetItemMock).toHaveBeenCalledWith('variable-statusCode', statusCode.toString())
+    expect(storeSetItemMock).toHaveBeenCalledWith('variable-ticket', body.ticketId)
+  })
+
+  it('should not read the response body or set any variables if no variable definitions are present', async () => {
+    storeGetItemMock.mockResolvedValue(null)
+
+    await variableSavingResponseHook(context)
+
+    expect(getBodyMock).not.toHaveBeenCalled()
+    expect(storeSetItemMock).not.toHaveBeenCalled()
+  })
+
+  it('should remove variable definitions after using them so that they do not get reused by a different request', async () => {
+    const VariableDefinition: VariableDefinition = {
+      variableName: 'ticket',
+      jsonPath: '$',
+    }
+    const body = {
+      someValue: 'test',
+      ticketId: '123',
+    }
+    storeGetItemMock.mockResolvedValue(JSON.stringify([VariableDefinition]))
+    getBodyMock.mockReturnValue(JSON.stringify(body))
+
+    await variableSavingResponseHook(context)
+
+    expect(storeRemoveItemMock).toHaveBeenCalledWith('variableDefinitions')
   })
 })
--- src/response-hook/response-hook.ts
+++ src/response-hook/response-hook.ts
@@ -1,3 +1,24 @@
+import jsonpath from 'jsonpath'
+import { VariableDefinition } from '../custom-header-format/variable-definition'
+
 export const variableSavingResponseHook: Insomnia.ResponseHook = async (context: Insomnia.ResponseHookContext) => {
-  await context.store.setItem('variable-statusCode', context.response.getStatusCode().toString())
+  const serializedDefinitions = await context.store.getItem('variableDefinitions')
+  await context.store.removeItem('variableDefinitions')
+  if (serializedDefinitions) {
+    const definitions = JSON.parse(serializedDefinitions) as VariableDefinition[]
+    const response = JSON.parse((context.response.getBody() || '').toString())
+    await extractVariablesFromResponse(definitions, response, context)
+  }
+}
+
+async function extractVariablesFromResponse(
+  definitions: VariableDefinition[],
+  response: unknown,
+  context: Insomnia.ResponseHookContext,
+) {
+  const promises = definitions.map(async def => {
+    const value = jsonpath.query(response, def.jsonPath)
+    await context.store.setItem(`variable-${def.variableName}`, value.toString())
+  })
+  await Promise.all(promises)
 }
