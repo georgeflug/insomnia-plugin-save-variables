6dbc8bdf1978a91655390d1480b683c6dbcb22f7
Richard White <georgeflug@gmail.com>
2021-03-25 21:04:52 -0500

    GH-9. Add test covering full flow of the logic

---
+++ src/full-flow.test.ts
@@ -0,0 +1,34 @@
+import { createCustomHeader } from './custom-header-format/custom-header-format'
+import { VariableDefinition } from './custom-header-format/variable-definition'
+import { createMockHeaders } from './insomnia/headers-mock'
+import { createMockStore } from './insomnia/store-mock'
+import { variableDeclarationHeaderRequestHook } from './request-hook/request-hook'
+import { variableSavingResponseHook } from './response-hook/response-hook'
+import { savedVariableTemplateTag } from './variable-template-tag/variable-template-tag'
+
+describe('Test through entire system', () => {
+  it('should save variable using request header and response hook', async () => {
+    const ticketNumber = 'ID-123'
+    const context = {
+      request: createMockHeaders(),
+      response: {
+        getBody: jest.fn().mockReturnValue(`{"ticket": "${ticketNumber}","other":"doesNotMatter"}`),
+      },
+      store: createMockStore(),
+    }
+    const variableDefinition: VariableDefinition = {
+      variableName: 'ticketNumber',
+      jsonPath: '$.ticket',
+    }
+    context.request.setHeader(createCustomHeader(variableDefinition), 'doesNotMatter')
+
+    await variableDeclarationHeaderRequestHook((context as unknown) as Insomnia.RequestHookContext)
+    await variableSavingResponseHook((context as unknown) as Insomnia.ResponseHookContext)
+    const result = await savedVariableTemplateTag.run(
+      (context as unknown) as Insomnia.TemplateRunContext,
+      'ticketNumber',
+    )
+
+    expect(result).toEqual(ticketNumber)
+  })
+})
