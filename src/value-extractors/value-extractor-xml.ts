218549361755d2da5c549534d5a45ed766ba439e
Richard White <georgeflug@gmail.com>
2023-02-24 23:25:12 -0600

    fix: rename xml to xpath

--- src/value-extractors/value-extractor-xml.ts
+++ src/value-extractors/value-extractor-xml.ts
@@ -4,7 +4,7 @@ import { select } from 'xpath'
 
 export const valueExtractorXml: ValueExtractor = {
   type: 'xml',
-  displayName: 'XML',
+  displayName: 'XPath',
   argumentName: 'XPath',
   extract: async (sourceValue: string, extractionArgument: string): Promise<string | null | undefined> => {
     const xmlDoc = new DOMParser().parseFromString(sourceValue)
